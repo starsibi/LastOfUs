a34fbe3b1b36f54553e83f015bab72b216e3b95f
Sibi <starsibi@gmail.com>
2022-02-02 21:23:59 +0530

    navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoaderComponent } from './loader/loader.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoaderComponent
+    LoaderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
