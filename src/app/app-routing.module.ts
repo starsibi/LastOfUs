b9f3e95275528b27b3a6cd19ebcedfa0cd0337ff
Sibi <starsibi@gmail.com>
2022-02-02 21:53:57 +0530

    added home and credit component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreditsComponent } from './credits/credits.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "home", redirectTo: "" },
+  { path: "credits", component: CreditsComponent }, 
+  { path: "**", redirectTo: "" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoaderComponent } from './loader/loader.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { CreditsComponent } from './credits/credits.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    CreditsComponent
   ],
   imports: [
     BrowserModule,
