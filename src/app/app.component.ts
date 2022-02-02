adf8ec4194a0a76d7b3e12b1753d32a56c971c6a
Sibi <starsibi@gmail.com>
2022-02-02 13:56:04 +0530

    Loader changes Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LastOfUs';
+  showLoader: boolean = true;
+
+  ngOnInit(){
+    this.init();
+  }
+ init() {
+  setTimeout(() => {
+    this.showLoader = false;
+  }, 7000); 
+ }
 }
