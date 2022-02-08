4da48095fd89fce52c3145efcc7a52137babb2ac
Sibi <starsibi@gmail.com>
2022-02-08 11:13:36 +0530

    Scroll Change

--- src/app/credits/credits.component.ts
+++ src/app/credits/credits.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Credits } from '../models/credits';
 import { ResponseFileReaderService } from '../_services/response-file-reader.service';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-credits',
@@ -9,15 +10,41 @@ import { ResponseFileReaderService } from '../_services/response-file-reader.ser
 })
 export class CreditsComponent implements OnInit {
   credits: Credits[] = [];
+
+  @ViewChild('scrollMe')
+  private myScrollContainer!: ElementRef;
+  position: number = 0;
+  subscription!: Subscription;
+
   constructor(private responseFileReaderService: ResponseFileReaderService) { }
+
 
   ngOnInit(): void {
     this.getData();
-    window.setInterval(function() {
-      window.scrollBy(0,10);
-    },2);
+    this.initScroll();
   }
 
+  initScroll(){
+    var ua = navigator.userAgent;
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|CriOS/i.test(ua)) {
+      const source = interval(1);
+      this.subscription = source.subscribe(val => this.scrollToBottom(this.position += .5));
+    } else {
+      const source = interval(4);
+      this.subscription = source.subscribe(val => this.scrollToBottom(this.position += 4));
+    }
+  }
+
+  scrollToBottom(position: number): void {
+    try {
+      console.log(position)
+      this.myScrollContainer.nativeElement.scrollTop = position;     
+    } catch (err) {}
+  }
+
+  scrollHandler() {  
+    this.subscription.unsubscribe();
+  }
   getData() {
     this.responseFileReaderService.getJSON("assets/data/credits.json").subscribe(data => {
       this.credits = data;
