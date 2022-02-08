ca9272bb2a26b624f5b7c8e8bfa1a2870055653f
Sibi <starsibi@gmail.com>
2022-02-08 09:42:06 +0530

    PWA Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { HomeComponent } from './home/home.component';
 import { CreditsComponent } from './credits/credits.component';
 import { NgParticlesModule } from 'ng-particles';
 import { FooterComponent } from './footer/footer.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,13 @@ import { FooterComponent } from './footer/footer.component';
     HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    NgParticlesModule 
+    NgParticlesModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }) 
   ],
   providers: [],
   bootstrap: [AppComponent]
